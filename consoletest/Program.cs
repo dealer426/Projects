0b4af3b3bc5e91d5e0cb7b75ce5e8c0208cc7724
dealer426 <burnssamuel01@gmail.com>
2020-01-13 09:30:28 -0500

    Commit

---
+++ consoletest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace consoletest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int x = 5;
+            int y = 2;
+
+           Console.WriteLine("When you add {0} + {1} it equals {2}", x, y,x+y);
+
+           Console.WriteLine("Here is a live coded Line!");
+
+            Console.ReadKey();  
+
+            
+
+        }
+    }
+}
